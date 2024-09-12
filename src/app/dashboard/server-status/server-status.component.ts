a5e43f02a1d91a1cba25bb093e51b787cd84b173
Moh Med <gmelk02@gmail.com>
2024-09-12 09:05:27 +0200

    Signal effect

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
   DestroyRef,
+  effect,
   inject,
   OnDestroy,
   OnInit,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -14,19 +16,26 @@ import {
   styleUrls: ['./server-status.component.css'],
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  //currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
   //private intervalId?: ReturnType<typeof setInterval>;
   private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+  }
 
   ngOnInit() {
     const intervalId = setInterval(() => {
       const random = Math.random();
       if (random < 0.5) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else if (random < 0.9) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
     this.destroyRef.onDestroy(() => {
