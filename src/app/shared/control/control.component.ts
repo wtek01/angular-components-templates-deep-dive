841a01d0bcaf9ed6c72e1a9223023773d5464c9a
El khanfousse Mohamed <wtek01@gmail.com>
2025-02-07 12:58:27 +0100

    Comments

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -21,6 +21,22 @@ import {
     '(click)': 'onClick()',
   },
 })
+
+/*1.Utilisation de @Host :
+- Le décorateur @Host permet d'appliquer automatiquement une classe CSS control à l'élément parent dans le template HTML
+- Au lieu d'écrire manuellement <app-control class="control">, la classe est automatiquement ajoutée à l'élément <app-control>
+- Cela permet de cibler et de styler l'élément parent directement
+2. Utilisation de @ContentChild :
+- @ContentChild est utilisé pour accéder aux éléments enfants projetés dans le template
+- Il permet de cibler spécifiquement les éléments qui ont l'attribut name="input"
+- Ces éléments sont accessibles via des propriétés privées de type ElementRef (comme control et control2)
+- On peut ensuite interagir avec ces éléments, par exemple en leur ajoutant des gestionnaires d'événements onClick()
+3. En résumé :
+- @Host gère le styling au niveau du composant parent
+- @ContentChild permet d'accéder et de manipuler les éléments enfants projetés
+- Les deux décorateurs facilitent l'interaction et le styling des composants de manière plus propre et maintenable
+Cette approche est plus élégante que d'ajouter manuellement des classes ou des gestionnaires d'événements directement
+dans le composant dans le template HTML.*/
 export class ControlComponent {
   //@HostBinding('class') className = 'control';
   /*@HostListener('click') onClick() {
